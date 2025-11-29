cc7dc7e1b54bd1847d90abbb143d81bf7a411332
Arafat Shalehin <md.arafatshalehin@gmail.com>
2025-11-29 20:15:32 +0600

    Added two parcel related apis

--- index.js
+++ index.js
@@ -25,6 +25,58 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("Zap-Shift-DB");
+    const parcelsCollection = db.collection("parcels");
+
+    // Parcel API
+    app.get("/parcels", async (req, res) => {
+      try {
+        const query = {};
+        const { email } = req.query;
+
+        if (email) {
+          query.senderEmail = email;
+        }
+
+        const cursor = parcelsCollection.find(query);
+        const result = await cursor.toArray();
+        res.status(201).json({
+          success: true,
+          message: "Parcel Found successfully.",
+          data: result,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          success: false,
+          message: "Couldn't find any Parcel.",
+        });
+      }
+    });
+
+    app.post("/parcels", async (req, res) => {
+      try {
+        const parcel = req.body;
+
+        const result = await parcelsCollection.insertOne(parcel);
+
+        res.status(201).json({
+          success: true,
+          message: "Parcel created successfully.",
+          data: result,
+        });
+      } catch (error) {
+        console.error("Parcel creation error:", error);
+
+        res.status(500).json({
+          success: false,
+          message: "Unable to create parcel at the moment.",
+          error: "INTERNAL_SERVER_ERROR", // high-level and safe
+        });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
